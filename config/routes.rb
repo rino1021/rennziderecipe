f145f892d13997bede2637010de930a9435143ef
rino1021 <krat1021007@gmail.com>
2022-08-03 11:18:40 +0000

    devise install ActiveStorage導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root :to => 'homes#top'
+  resources :post_images, only: [:new, :index, :show,:edit, :update,:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

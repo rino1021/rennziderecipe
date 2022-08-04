89a34d68958335943370f0e332c507f14ee7f5d7
rino1021 <krat1021007@gmail.com>
2022-08-04 08:20:26 +0000

    マイページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root :to => 'homes#top'
-  resources :post_images, only: [:new, :index, :show,:edit, :update,:create]
+  resources :post_images, only: [:new, :index, :show,:edit, :update,:create,:destroy]
+  resources :users, only:[:show,:edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

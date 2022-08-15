3336226cf7a0492394353a3af910d1d471b0963d
rino1021 <krat1021007@gmail.com>
2022-08-15 08:43:21 +0000

    indecレイアウト

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -16,7 +16,7 @@ class RelationshipsController < ApplicationController
   # フォロー一覧
   def followings
     @user = User.find(params[:user_id])
-    @users = user.followings
+    @users = @user.followings
   end
   # フォロワー一覧
   def followers

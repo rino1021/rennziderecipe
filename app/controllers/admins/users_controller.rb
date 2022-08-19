f8f59975f6bfa51b8f76e2cbb62cfc59411addd1
rino1021 <krat1021007@gmail.com>
2022-08-19 10:33:17 +0000

    管理者機能

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -6,4 +6,41 @@ class Admins::UsersController < ApplicationController
     #@user = User.find(params[:id])
     #@post_images = current_user.post_images
   end
+
+  def show
+    #favorites
+     @user = User.find(params[:id])
+     favorites= Favorite.where(user_id: @user.id).pluck(:post_image_id)
+     @favorite_post_images = PostImage.find(favorites)
+     @post_images = @user.post_images
+
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    #@user.update(user_params)
+    #redirect_to user_path
+    if@user.update(user_params)
+    redirect_to user_path(@user.id),notice:"You have updated user successfully."
+    else
+    render:edit
+    end
+  end
+
+  def favorites
+    @user = User.find(params[:id])
+    favorites= Favorite.where(user_id: @user.id).pluck(:post_image_id)
+    @favorite_post_images = PostImage.find(favorites)
+    #@post_imags = @user.post_image
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
 end
+
